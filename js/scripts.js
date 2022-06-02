75a9394d74bd9defcef2eeb1c89ff4cdc3420ec6
Aleleonel <aleleonel@gmail.com>
2022-06-02 10:49:51 -0300

    validando campo cep

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-console.log("deu certo")
+const addressForm = document.querySelector("#address-form"); 
+const cepInput = document.querySelector("#cep");
+const adressInput = document.querySelector("#address");
+const cityInput = document.querySelector("#city");
+const neighborhoodInput = document.querySelector("#neighborhood");
+const regionInput = document.querySelector("#region");
+const formInputs = document.querySelectorAll("[data-input]");
+
+const closeButton = document.querySelector("#close-message");
+
+// validate cep input
+
+cepInput.addEventListener("keypress", (e) => {
+    const onlyNumbers = /[0-9]/;
+    const key = String.fromCharCode(e.keyCode);
+
+    if (!onlyNumbers.test(key)) {
+        e.preventDefault();
+        return
+    }
+});
